69d1dedd155c97a3766d1dc9337af27ae9c698b2
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-27 18:21:12 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mock;
+using Moq;
+using FluentAssertions;
+using System.Collections.Generic;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MethodTest()
+        {
+           var listOfBands = new List<Band>();
+            listOfBands.Add(new Band
+            {
+                Name = "Insync Maniacs",
+                song = "Fir Kabhi",
+                genre = "emotional"
+            });
+
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+            mockRepository.Setup(x => x.GetBands()).Returns(listOfBands);
+            var bandController = new BandController(mockRepository.Object);
+            bandController.Method();
+            bandController.Should().NotBeNull();
+        }
+    }
+}
